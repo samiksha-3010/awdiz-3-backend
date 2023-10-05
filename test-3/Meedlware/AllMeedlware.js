93efcaf730ec329b5bb1a9ddd52e7157c4449d74
samiksha-3010 <samiksha30102@gmail.com>
2023-10-05 12:03:16 +0530

    done

--- test-3/Meedlware/AllMeedlware.js
+++ test-3/Meedlware/AllMeedlware.js
@@ -22,3 +22,55 @@ export const checkSeller = async (req,res)=>{
     
    } 
 }
+
+export const isAdmin = async (req, res, next) => {
+    try {
+        const { token } = req.body;
+        if (!token) return res.status(404).json({ success: false, message: "Token is mandtory.." })
+
+        const decodedData = jwt.verify(token, process.env.JWT_SECRET)
+
+        if (!decodedData) {
+            return res.status(404).json({ success: false, message: "Token not valid." })
+        }
+
+        const userId = decodedData.userId;
+
+        const user = await UserModal.findById(userId);
+
+        if (!user || user?.role != "Admin") {
+            return res.status(404).json({ message: "User is not an Admin!", success: false })
+        }
+
+        next();
+
+    } catch (error) {
+        return res.status(500).json({ error: error.message, success: false })
+    }
+}
+
+export const isValidUser = async(req,res,next)=>{
+    try {
+        const { token } = req.body;
+        if (!token) return res.status(404).json({ success: false, message: "Token is mandtory.." })
+
+        const decodedData = jwt.verify(token, process.env.JWT_SECRET)
+
+        if (!decodedData) {
+            return res.status(404).json({ success: false, message: "Token not valid." })
+        }
+
+        const userId = decodedData.userId;
+
+        const user = await UserModal.findById(userId);
+
+        if (!user ) {
+            return res.status(404).json({ message: "User is not Valid!", success: false })
+        }
+
+        next();
+
+    } catch (error) {
+        return res.status(500).json({ error: error.message, success: false })
+    }
+}
